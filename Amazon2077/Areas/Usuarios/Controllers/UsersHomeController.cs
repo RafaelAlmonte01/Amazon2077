516c949304a230b99580557cf724c178bc632a75
RafaelAlmonte01 <62627526+RafaelAlmonte01@users.noreply.github.com>
2021-05-21 19:17:56 -0400

    Agregar archivos de proyecto.

---
+++ Amazon2077/Areas/Usuarios/Controllers/UsersHomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amazon2077.Areas.Usuarios.Controllers
+{
+    [Area("Usuarios")]
+    public class UsersHomeController : Controller
+    {
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
---
+++ Amazon2077/Areas/Usuarios/Models/InputModelRegister.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amazon2077.Areas.Usuarios.Models
+{
+    public class InputModelRegister
+    {
+
+
+        [Required (ErrorMessage = "Este Campo es obligatorio.") ]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Este Campo es obligatorio.")]
+        public string Apellido { get; set; }
+
+        public string NID { get; set; }
+
+
+
+    }
+}
